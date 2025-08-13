b75de14aebf2280962c990d198463b595ae5ee68
bahmN <iwanaev23@gmail.com>
2025-08-13 15:29:06 +0300

    dark mode disabled

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -3,17 +3,12 @@
 namespace App\Providers\Filament;
 
 use Filament\Pages\Dashboard;
-use Filament\Widgets\AccountWidget;
-use Filament\Widgets\FilamentInfoWidget;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\AuthenticateSession;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
-use Filament\Pages;
 use Filament\Panel;
 use Filament\PanelProvider;
-use Filament\Support\Colors\Color;
-use Filament\Widgets;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 use Illuminate\Cookie\Middleware\EncryptCookies;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
@@ -35,6 +30,7 @@ class AdminPanelProvider extends PanelProvider {
                 'success'  => '#71be38',
                 'warning'  => '#f59e0b',
             ])
+            ->darkMode(false)
             ->brandName("Цветофор.рф")
             ->brandLogo(asset('images/logo.svg'))
             ->brandLogoHeight('3rem')
