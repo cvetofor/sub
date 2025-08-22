2c86c57e9c88bdfb404aa44f4851b6cb78365d33
bahmN <iwanaev23@gmail.com>
2025-08-22 23:53:24 +0300

    создал модель для статусов оплаты, сделал взаимодействие с моделью Payment для получения статусов оплаты

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -12,4 +12,12 @@ class Payment extends Model {
         'payment_status_id',
         'payment_gateway_transaction'
     ];
+
+    public function status() {
+        return $this->belongsTo(PaymentStatus::class, 'payment_status_id');
+    }
+
+    public function statusName(): string {
+        return $this->status?->name ?? 'Неизвестный статус';
+    }
 }
