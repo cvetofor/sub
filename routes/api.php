d47806bf715ed83a701dafbc0697b8dbbe438dc3
bahmN <iwanaev23@gmail.com>
2025-08-22 19:31:38 +0300

    роут для колбеков от Юкассы

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,15 @@
 <?php
 
+use App\Http\Api\Yookassa;
 use App\Http\Controllers\SubscriptionController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => '/subscription', 'as' => 'subscription.'], function () {
     Route::post('/create', [SubscriptionController::class, 'create'])->name('create');
+});
+
+Route::group(['prefix' => '/payment', 'as' => 'payment.'], function () {
+    Route::group(['prefix' => '/yookassa', 'as' => 'yookassa.'], function () {
+        Route::post('/callback', [Yookassa::class, 'callback']);
+    });
 });
