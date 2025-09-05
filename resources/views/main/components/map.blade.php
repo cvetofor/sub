caaca63fbe63081479bf04ebd6c1e9da42d50555
bahmN <iwanaev23@gmail.com>
2025-09-05 14:09:40 +0300

    скруглены края карты и установлен цвет

--- resources/views/main/components/map.blade.php
+++ resources/views/main/components/map.blade.php
@@ -1,6 +1,6 @@
 <section class="flex flex-col items-center mx-auto max-w-7xl px-4 pb-14 gap-4">
     <h1 class="text-xl font-semibold mb-2">Наш Адрес</h1>
-    <div class="w-full">
+    <div class="w-full flex flex-col gap-2">
         <a class="dg-widget-link"
             href="http://2gis.ru/ulanude/firm/70000001094519097/center/107.60267257690431,51.815460039741914/zoom/15?utm_medium=widget-source&utm_campaign=firmsonmap&utm_source=bigMap">Посмотреть
             на карте Улан-Удэ</a>
@@ -10,26 +10,27 @@
         <div class="dg-widget-link"><a
                 href="http://2gis.ru/ulanude/center/107.603465,51.811613/zoom/15/routeTab/rsType/bus/to/107.603465,51.811613╎Цветофор, служба доставки цветов?utm_medium=widget-source&utm_campaign=firmsonmap&utm_source=route">Найти
                 проезд до Цветофор, служба доставки цветов</a></div>
-        <script charset="utf-8" src="https://widgets.2gis.com/js/DGWidgetLoader.js"></script>
-        <script charset="utf-8">
-            new DGWidgetLoader({
-                "width": "100%",
-                "height": 500,
-                "borderColor": "#a3a3a3",
-                "pos": {
-                    "lat": 51.815460039741914,
-                    "lon": 107.60267257690431,
-                    "zoom": 15
-                },
-                "opt": {
-                    "city": "ulanude"
-                },
-                "org": [{
-                    "id": "70000001094519097"
-                }]
-            });
-        </script><noscript style="color:#c00;font-size:16px;font-weight:bold;">Виджет карты использует
-            JavaScript.
-            Включите его в настройках вашего браузера.</noscript>
+ 
+        <div class="rounded-3xl border border-rose-200 overflow-hidden mt-2">
+            <script charset="utf-8" src="https://widgets.2gis.com/js/DGWidgetLoader.js"></script>
+            <script charset="utf-8">
+                new DGWidgetLoader({
+                    "width": "100%",
+                    "height": 500,
+                    "borderColor": "#ffe4e6",
+                    "pos": {
+                        "lat": 51.815460039741914,
+                        "lon": 107.60267257690431,
+                        "zoom": 15
+                    },
+                    "opt": {
+                        "city": "ulanude"
+                    },
+                    "org": [{
+                        "id": "70000001094519097"
+                    }]
+                });
+            </script>
+        </div>
     </div>
 </section>
